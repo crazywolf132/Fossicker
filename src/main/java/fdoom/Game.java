7b5907e654e57a64e39b895f962c735f68fb87b8
crazywolf132 <crazywolf132@gmail.com>
2019-03-07 15:24:47 +1100

    Started Game file
    Added an error handler to begin with

--- src/main/java/fdoom/Game.java
+++ src/main/java/fdoom/Game.java
@@ -1,4 +1,31 @@
 package fdoom;
 
+import javax.swing.*;
+import java.io.PrintWriter;
+import java.io.StringWriter;
+
 public class Game {
+
+    Game() {}
+
+    static boolean running = true;
+
+    public static void quit() {
+        running = false;
+    }
+
+    public static void main(String[] args) {
+        Thread.setDefaultUncaughtExceptionHandler((thread, throwable) -> {
+            throwable.printStackTrace();
+
+            StringWriter string = new StringWriter();
+            PrintWriter printer = new PrintWriter(string);
+            throwable.printStackTrace(printer);
+
+            JTextArea errorDisplay = new JTextArea(string.toString());
+            errorDisplay.setEditable(false);
+            JScrollPane errorPane = new JScrollPane(errorDisplay);
+            JOptionPane.showMessageDialog(null, errorPane, "An error has occurred", JOptionPane.ERROR_MESSAGE);
+        });
+    }
 }
